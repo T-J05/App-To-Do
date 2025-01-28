f4a4c8bec7cb048128eed781b9dfa3bcf256fd5f
Elias <eliasvera793@gmail.com>
2025-01-27 21:05:41 -0300

    Implementacion sencilla de app To-do

---
+++ app.js
@@ -0,0 +1,51 @@
+// index.js
+import { createElement, render, createStore } from './src/frame.js'; // Ruta relativa a simpleJSX.js
+console.log(createStore, "haso")
+// Ahora puedes usar createElement, render, y createStore
+const reducer = (state = { todos: [] }, action) => {
+    switch (action.type) {
+        case 'ADD_TODO':
+            return { ...state, todos: [...state.todos, action.payload] };
+        default:
+            return state;
+    }
+};
+
+const store = createStore(reducer);
+
+const renderApp = () => {
+    const state = store.getState();
+
+    const app = createElement(
+        'div',
+        null,
+        createElement('h1', null, 'Lista de Tareas'),
+        createElement(
+            'ul',
+            null,
+            ...state.todos.map(todo => createElement('li', null, todo))
+        ),
+        createElement(
+            'input',
+            { id: "input-todo", placeholder: "Escribe una tarea" }
+        ),
+        createElement(
+            'button',
+            {
+                onclick: () => {
+                    const task = document.getElementById("input-todo");
+                    if (task && task.value) {
+                        store.dispatch({ type: 'ADD_TODO', payload: task.value });
+                        task.value = '';
+                    }
+                }
+            },
+            'Agregar Tarea'
+        )
+    );
+
+    render(app, document.getElementById('root'));
+};
+
+store.subscribe(renderApp);
+renderApp();
